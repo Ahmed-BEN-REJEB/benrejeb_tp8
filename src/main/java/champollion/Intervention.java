d9754d2651429c28077be7e05846c29aa298b754
Ahmed-BEN-REJEB <ahmed.ben_rejeb@etud.univ-jfc.fr>
2024-11-20 17:35:48 +0100

    "modifie"

---
+++ src/main/java/champollion/Intervention.java
@@ -0,0 +1,40 @@
+package champollion;
+import java.util.*;
+import java.lang.*;
+import lombok.Getter;
+import lombok.Setter;
+
+public class Intervention {
+    
+    //Définition des attributs
+    @Getter
+    @Setter
+    private Date dateDebut;
+
+    @Getter
+    @Setter
+    private int heureDebut;
+
+    @Getter
+    @Setter
+    private int duree=0;
+
+    @Getter
+    @Setter
+    private boolean annulee=false;
+
+    @Getter
+    @Setter
+    private TypeIntervention type_Intervention;
+
+    @Getter
+    @Setter
+    private UE uniteEnseignement;
+
+    //Définition du constructeur
+    Intervention(UE uniteEnseignement, TypeIntervention type_Intervention, int duree){
+        this.uniteEnseignement = uniteEnseignement;
+        this.type_Intervention = type_Intervention;
+        this.duree = duree;
+    }
+}
